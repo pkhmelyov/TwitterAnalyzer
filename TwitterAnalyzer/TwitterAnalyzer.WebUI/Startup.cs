c089ae2aa3c366191c2a2e6a8963403eb1b34368
Pavel Khmelyov <pavel.khmelyov@arcadia.spb.ru>
2017-08-11 17:28:07 +0300

    Initial Commit

---
+++ TwitterAnalyzer/TwitterAnalyzer.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TwitterAnalyzer.WebUI.Startup))]
+namespace TwitterAnalyzer.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
